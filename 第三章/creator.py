276ef85531986a3131188f7d36c3bc55628934e3
Cha <x@x.com>
2023-11-02 01:29:46 +0800

    2023年11月2日 01:29:45

---
+++ 第三章/creator.py
@@ -0,0 +1,12 @@
+def get_creators(record: dict) -> list:
+    match record:
+        case {'type': 'book', 'api': 2, 'authors': [*names]}:
+            return names
+        case {'type': 'book', 'api': 1, 'author': name}:
+            return [name]
+        case {'type': 'book'}:
+            raise ValueError(f"Invalid 'book' record: {record!r}")
+        case {'type': 'movie', 'director': name}:
+            return [name]
+        case _:
+            raise ValueError(f'Invalid record: {record!r}')
